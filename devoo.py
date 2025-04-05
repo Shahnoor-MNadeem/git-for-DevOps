528e08e939fa6b8019fdc6fa18b6599dfbbf4331
Shahnoor Nadeem <shaninad32@gmail.com>
2025-04-05 17:39:56 +0500

    sending changes to check action

--- devoo.py
+++ devoo.py
@@ -1,1 +1,2 @@
+
 print("let's test our dev branch I created to see to merge both branches together ")
--- gitz.py
+++ gitz.py
@@ -2,7 +2,7 @@
 
 print("hello world ")
 
-this is the second line i am going to push 
+print("this is the second line i am going to push ")
 
 print("let's run the pull command ")
 
