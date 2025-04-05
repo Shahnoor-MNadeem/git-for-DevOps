e0f9a19e8fc3e07fce15908b8cb2c39778e8a1b5
Shahnoor Nadeem <shaninad32@gmail.com>
2025-04-05 17:46:25 +0500

    add vulnerable code in py files

--- devoo.py
+++ devoo.py
@@ -3,3 +3,7 @@ This module prints a test message to demonstrate DevOps workflow
 and testing of code in a development branch.
 """
 print("let's test our dev branch I created to see to merge both branches together ")
+
+
+if b == "123":
+    print("right")
--- gitz.py
+++ gitz.py
@@ -9,3 +9,8 @@ print("this is the second line i am going to push ")
 print("let's run the pull command ")
 
 print("Apple ")
+
+a = 23
+
+if a == "123":
+    print("right")
